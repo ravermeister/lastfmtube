90df3730a66726a849a9831df6e2d6ae2a8e6b17
Jonny Rimkus <jonny@rimkus.it>
2017-08-31 02:34:25 +0200

    pdo auf prepared statements gewechselt

--- php/charts.php
+++ php/charts.php
@@ -9,47 +9,7 @@ switch($_POST['action'])
     break;
     
     case 'show' :
-        /*
-         // TOP 100 
-         $select_sql =
-         '
-            SELECT *  
-            FROM  `site_music_charts`  
-            ORDER BY  `playcount` DESC, `lastplay_time` DESC;  
-            LIMIT 0,100;
-         ';
-         
-         */
-         // All Tracks playcount > 1
-         /*
-             SELECT * 
-             FROM `site_music_charts` 
-             WHERE 
-             	(`playcount` > 1) OR 
-                (
-                    `playcount`=1 AND 
-                    `lastplay_time` IN (
-                        SELECT MAX(`lastplay_time`) 
-                        FROM `site_music_charts` 
-                        WHERE `playcount`=1
-                    )
-    			)
-             ORDER BY `playcount` DESC, `lastplay_time` DESC;
-         */         
-	$prefix = $settings['database']['table_prefix'];
-	$select_sql = '
-		SELECT * FROM "'.$prefix.'charts" 
-		WHERE ("playcount" > 1) OR (
-                    "playcount" = 1 AND 
-                    "lastplay_time" IN (
-                        SELECT MAX("lastplay_time") 
-                        FROM "'.$prefix.'charts"
-                        WHERE "playcount"=1
-                    )
-    			)
-             ORDER BY `playcount` DESC, `lastplay_time` DESC;
-	 ';
-         $data = DB::getInstance()->query($select_sql);            
+	$data = DB::getInstance()->query('SELECT_CHARTS');            
         echo json_encode($data); 
     break;
 }
