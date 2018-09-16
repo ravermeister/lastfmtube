d71f34de0e4089be94b48836587d79ab2a1a8ee3
Jonny Rimkus <jonny@rimkus.it>
2018-09-16 02:02:20 +0200

    ....

--- index.php
+++ index.php
@@ -17,4 +17,9 @@ $baseURL   = $settings['general']['baseurl'];
 $themeData = file_get_contents('themes/'.$themeFile.'/'.$themeFile.'.html');
 $themeData = str_replace('{{LANG}}', $settings['general']['lang'], $themeData);
 $themeData = str_replace('{{TITLE}}', 'Last.fm Youtube Radio', $themeData);
+
+header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+header("Pragma: no-cache"); // HTTP 1.0.
+header("Expires: 0"); // Proxies.
+header("Content-Type: text/html"); //html content
 die($themeData);
