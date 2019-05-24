1bb6917aa2c83ade1616fbca5beae7c85c540e6a
Jonny Rimkus <jonny@rimkus.it>
2019-05-24 22:54:30 +0200

    ..

--- php/json/page/Playlist.php
+++ php/json/page/Playlist.php
@@ -217,6 +217,7 @@ class Playlist extends DefaultJson {
 
 		$page ['TRACKS'] = array_values($uniqueTracks);
 		Functions::getInstance()->logMessage(print_r($page['TRACKS'], true));
+		Functions::getInstance()->logMessage(print_r($uniqueTracks, true));
 		return $page;
 	}
 	private function getTopUser($pageNum = 1, $user = false) {
