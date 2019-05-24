975998c27fbb7fc40eeba72ab6200847c302b0e3
Jonny Rimkus <jonny@rimkus.it>
2019-05-24 22:33:50 +0200

    .

--- php/json/page/Playlist.php
+++ php/json/page/Playlist.php
@@ -187,7 +187,7 @@ class Playlist extends DefaultJson {
 			$trackId = $track ['artist'] . '-' . $track ['title'];
 			if (array_key_exists ( $trackId, $uniqueTracks )) {
 				$uniqueTrack = $uniqueTracks [$trackId];
-				Functions::getInstance ()->logMessage ( $trackId . ' Old=' . $uniqueTrack ['PLAYCOUNT'] . ' Dup=' . $track ['playcount'] . ' New=' . (( int ) $uniqueTrack ['PLAYCOUNT']) + (( int ) $track ['playcount']) );
+				Functions::getInstance ()->logMessage ( '>>>>'. $trackId . ' Old=' . $uniqueTrack ['PLAYCOUNT'] . ' Dup=' . $track ['playcount'] . ' New=' . (( int ) $uniqueTrack ['PLAYCOUNT']) + (( int ) $track ['playcount']) );
 				$uniqueTrack ['PLAYCOUNT'] = (( int ) $uniqueTrack ['PLAYCOUNT']) + (( int ) $track ['playcount']);
 				continue;
 			}
