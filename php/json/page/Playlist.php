1ca93a5a545cef0a8118078ac9bff0f982296e87
Jonny Rimkus <jonny@rimkus.it>
2019-05-26 20:28:51 +0200

    .

--- php/json/page/Playlist.php
+++ php/json/page/Playlist.php
@@ -137,7 +137,7 @@ class Playlist extends DefaultJson {
         $sort_bydate = $locale ['playlist'] ['control'] ['sortby'] ['date'];
         $sort_bypcount = $locale ['playlist'] ['control'] ['sortby'] ['playcount'];
 
-        if ($sortby === false || ! (strcmp ( $sortby, $sort_bydate ) == 0 || strcmp ( $sort_bypcount ) == 0)) {
+        if ($sortby === false || ! (strcmp ( $sortby, $sort_bydate ) == 0 || strcmp ( $sortby, $sort_bypcount ) == 0)) {
             $sortby = $locale ['playlist'] ['control'] ['sortby'] ['playcount'];
         }
         
