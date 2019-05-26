15a0d7b568359656d0575739d66132a7f64c29a9
Jonny Rimkus <jonny@rimkus.it>
2019-05-26 16:00:27 +0200

    .

--- js/lib/libvue/libvue-video.js
+++ js/lib/libvue/libvue-video.js
@@ -133,7 +133,7 @@ class LibvueVideo {
             methods: {
             	normalizeMessage: function(comments) {
             		for(let comment in comments) {
-            			let links = $.parseHTML(comment).find('a');
+            			let links = $.parseHTML(comment);
             			console.log('links:');
             			console.log(links);
             		}
