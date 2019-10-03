982148f883c01d6a2185eab2233e8a4fc8449d2d
Jonny Rimkus <jonny@rimkus.it>
2019-10-03 18:26:52 +0200

    getting closer

--- php/api/LastFm/Track.php
+++ php/api/LastFm/Track.php
@@ -178,11 +178,11 @@ class Track
                 }
 
                 $this->title = preg_replace($orig_title_expr, $repl_title, $this->title);
-                //Functions::getInstance()->logMessage('title replacement: '.$this->title);
+                Functions::getInstance()->logMessage('title replacement: '.$this->title);
                 
                 $repl_title = str_replace(DB::$ARTIST_REPLACEMENT_REGEX_IDENTIFIER, '$', $this->title);
                 $repl_artist_title = preg_replace($orig_artist_expr, $repl_title, $this->artist);
-                //Functions::getInstance()->logMessage('Artist after title: '.$this->artist);
+                Functions::getInstance()->logMessage('title after artist: '.$this->artist);
                 
                 if (strpos($this->artist, DB::$ARTIST_REPLACEMENT_REGEX_IDENTIFIER) !== false /*&& strcmp($repl_artist_title, $this->artist) !== 0*/) {
                     $this->title = $repl_artist_title;
