41766c5c7963562182e6aa10546de0020abeaa3f
Jonny Rimkus <jonny@rimkus.it>
2019-10-03 07:10:55 +0200

    and another try.,.

--- php/api/LastFm/Track.php
+++ php/api/LastFm/Track.php
@@ -170,11 +170,18 @@ class Track
             if (preg_match($orig_artist_expr, $this->artist) === 1 && preg_match($orig_title_expr, $this->title) === 1) {
                 $this->artist = preg_replace($orig_artist_expr, $repl_artist, $this->artist);              
                 $repl_artist = str_replace(DB::$TITLE_REPLACEMENT_REGEX_IDENTIFIER, '$', $row['repl_artist']);                
-                $this->artist = preg_replace($orig_title_expr, $repl_artist, $this->title);
+                $repl_title_artist = preg_replace($orig_title_expr, $repl_artist, $this->title);
+                if(strcmp($repl_title_artist, $this->title) !== 0) {
+                    $this->artist = $repl_title_artist;
+                }
+                    
 
                 $this->title = preg_replace($orig_title_expr, $repl_title, $this->title);
                 $repl_title = str_replace(DB::$ARTIST_REPLACEMENT_REGEX_IDENTIFIER, '$', $row['repl_title']);
-                $this->title = preg_replace($orig_artist_expr, $repl_title, $this->artist);
+                $repl_artist_title = preg_replace($orig_artist_expr, $repl_title, $this->artist);
+                if(!strcmp($repl_artist_title, $this->artist)) {
+                    $this->title = $repl_artist_title;
+                }
             }
         }
     }
