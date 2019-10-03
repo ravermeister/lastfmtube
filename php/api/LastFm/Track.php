7436f9821b38b2db0c51702c288b7384c72efd20
Jonny Rimkus <jonny@rimkus.it>
2019-10-03 18:29:56 +0200

    .

--- php/api/LastFm/Track.php
+++ php/api/LastFm/Track.php
@@ -172,21 +172,28 @@ class Track
                 
                 $repl_artist = str_replace(DB::$TITLE_REPLACEMENT_REGEX_IDENTIFIER, '$', $this->artist);
                 //Functions::getInstance()->logMessage('repl artist: '.$repl_artist);
+                
                 $repl_title_artist = preg_replace($orig_title_expr, $repl_artist, $this->title);
                 if (strpos($this->artist, DB::$TITLE_REPLACEMENT_REGEX_IDENTIFIER) !== false /*&& strcmp($repl_title_artist, $this->title) !== 0*/) {
                     $this->artist = $repl_title_artist;
                 }
-
+                //title replaced
+                
+                
                 $this->title = preg_replace($orig_title_expr, $repl_title, $this->title);
                 Functions::getInstance()->logMessage('title replacement: '.$this->title);
                 
                 $repl_title = str_replace(DB::$ARTIST_REPLACEMENT_REGEX_IDENTIFIER, '$', $this->title);
+                Functions::getInstance()->logMessage('repl_title: '.$repl_title);
+                
                 $repl_artist_title = preg_replace($orig_artist_expr, $repl_title, $this->artist);
                 Functions::getInstance()->logMessage('title after artist: '.$this->title);
                 
                 if (strpos($this->artist, DB::$ARTIST_REPLACEMENT_REGEX_IDENTIFIER) !== false /*&& strcmp($repl_artist_title, $this->artist) !== 0*/) {
                     $this->title = $repl_artist_title;
                 }
+                //artist replaced
+                
                 
                 // stop prcessing when pattern matched
                 break;
