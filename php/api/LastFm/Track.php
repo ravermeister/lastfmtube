1ec924b1aa32bebf6a7990e6c25a5060da7f6b64
Jonny Rimkus <jonny@rimkus.it>
2019-10-03 06:38:56 +0200

    first try

--- php/api/LastFm/Track.php
+++ php/api/LastFm/Track.php
@@ -158,23 +158,24 @@ class Track
     public function normalize()
     {
         $replacements = Db::getInstance()->getReplaceTrackMap();
-    
+
         foreach ($replacements as $row) {
-            
+
             $orig_artist_expr = '/' . $row['orig_artist_expr'] . '/';
             $orig_title_expr = '/' . $row['orig_title_expr'] . '/';
             $repl_artist = str_replace(DB::$ARTIST_REPLACEMENT_REGEX_IDENTIFIER, '$', $row['repl_artist']);
             $repl_artist = str_replace(DB::$TITLE_REPLACEMENT_REGEX_IDENTIFIER, '$', $repl_artist);
-            
+
             $repl_title = str_replace(DB::$TITLE_REPLACEMENT_REGEX_IDENTIFIER, '$', $row['repl_title']);
             $repl_title = str_replace(DB::$ARTIST_REPLACEMENT_REGEX_IDENTIFIER, '$', $repl_title);
-            
-            if (preg_match($orig_artist_expr, $this->artist) === 1 && preg_match($orig_title_expr, $this->title) === 1) {                
-                $this->artist = preg_replace($orig_artist_expr, $repl_artist, $this->artist);                
-                //$this->artist = preg_replace($orig_title_expr, $repl_artist, $this->title);
+
+            if (preg_match($orig_artist_expr, $this->artist) === 1 && preg_match($orig_title_expr, $this->title) === 1) {
+                $this->artist = preg_replace($orig_artist_expr, $repl_artist, $this->artist);
                 
-                $this-> title = preg_replace($orig_title_expr, $repl_title, $this->title);
-                //$this-> title = preg_replace($orig_artist_expr, $repl_title, $this->artist);
+                $this->artist = preg_replace($orig_title_expr, $repl_title, $this->title);
+
+                $this->title = preg_replace($orig_title_expr, $repl_title, $this->title);
+                $this->title = preg_replace($orig_artist_expr, $repl_artist, $this->artist);
             }
         }
     }
