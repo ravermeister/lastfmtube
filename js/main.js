a9da3318660db19906978ffdf9c274f24b43e71f
Jonny Rimkus <jonny@rimkus.it>
2019-10-06 00:03:07 +0200

    .

--- js/main.js
+++ js/main.js
@@ -8,6 +8,9 @@ require([ 'Vue', 'Storages', 'player', 'page', 'playlist' ], function(Vue,
 	$playlist = new PlaylistController();
 	$page = new PageController();
 	
+	console.log('>>> href: >'+location.href+'<');
+	console.log('>>> path: >'+location.pathname+'<');
+	
 	$player.autoPlay = true;
 	$player.initPlayer();
 	$page.init();
