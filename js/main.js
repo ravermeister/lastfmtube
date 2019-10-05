92b019ddb9fb5423b6cebd9aa259ab7a77bec00d
Jonny Rimkus <jonny@rimkus.it>
2019-10-06 01:42:02 +0200

    d

--- js/main.js
+++ js/main.js
@@ -8,18 +8,15 @@ require([ 'Vue', 'Storages', 'player', 'page', 'playlist' ], function(Vue,
 	$playlist = new PlaylistController();
 	$page = new PageController();
 
-	$player.autoPlay = true;
-	$page.init(function() {
-		$playlist.loadLastFmList(1, null, function() {
-			
-			// maybe set it to page...
-			require([ 'analytics' ], function(analytics) {
-				PageController.analytics = analytics;
-			});
+//	$player.autoPlay = true;
+	$page.init();
+	$playlist.loadLastFmList();
 
-			$player.initPlayer();
-			
-			$page.initURL();
-		});
+	// maybe set it to page...
+	require([ 'analytics' ], function(analytics) {
+		PageController.analytics = analytics;
 	});
+	
+	$player.initPlayer();
+	$page.initURL();
 });
