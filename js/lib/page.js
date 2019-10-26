c6b2cba6dea9d59bc80b94063d44e03e5a667236
Jonny Rimkus <jonny@rimkus.it>
2019-10-27 01:31:34 +0200

    toggle is shit

--- js/lib/page.js
+++ js/lib/page.js
@@ -579,11 +579,11 @@ class PageController {
     		$player.loadNextSong();    		
     	});
     	
-    	hotkeys('space', function(event, handler){
-    		// Prevent the default refresh event under WINDOWS system
-    		event.preventDefault(); 
-    		$player.togglePlay();    		
-    	});
+//    	hotkeys('space', function(event, handler){
+//    		// Prevent the default refresh event under WINDOWS system
+//    		event.preventDefault(); 
+//    		$player.togglePlay();    		
+//    	});
 
     	hotkeys('enter', function(event, handler){
     		// Prevent the default refresh event under WINDOWS system
