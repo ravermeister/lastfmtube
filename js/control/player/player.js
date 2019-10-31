909ab7de8c756bedd30fb4e1e53d8b1605a1ac26
Jonny Rimkus <jonny@rimkus.it>
2019-10-31 20:21:44 +0100

    .

--- js/control/player/player.js
+++ js/control/player/player.js
@@ -101,7 +101,7 @@ class PlayerController {
         if (tracks.length === 0) return;
         let curTrack = this.currentTrackData.track;
         if(curTrack !== null) {
-        	console.log('!!!!', parseInt(curTrack.NR), (parseInt(curTrack.NR) % $page.settings.general.tracks_perpage));
+        	console.log('!!!!', parseInt(curTrack.NR), $page.settings.general.tracks_perpage);
         }
         
         let nextIndex = curTrack !== null ?
