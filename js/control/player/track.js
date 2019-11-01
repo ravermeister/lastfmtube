f5dd1ba3635192cb8d921854323357ed39ee62a1
Jonny Rimkus <jonny@rimkus.it>
2019-11-01 17:20:21 +0100

    fixed header

--- js/control/player/track.js
+++ js/control/player/track.js
@@ -1,9 +1,11 @@
 /*******************************************************************************
- * Created 2017,2019 by Jonny Rimkus <jonny@rimkus.it>. Hope you like it :)
- * 
- * Contributors: Jonny Rimkus - initial API and implementation
- ******************************************************************************/
-
+ * Created 2017,2019 by Jonny Rimkus <jonny@rimkus.it>.
+ * Hope you like it :)
+ *
+ * Contributors:
+ *     Jonny Rimkus - initial API and implementation
+ *******************************************************************************/
+/***/
 class TrackData {
 
 	constructor(){
