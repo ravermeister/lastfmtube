e57dbc5a0bd3709cca489f71dd1974d3afffa59d
Jonny Rimkus <jonny@rimkus.it>
2019-11-02 14:52:10 +0100

    buuugs

--- js/control/playlist/playlist.js
+++ js/control/playlist/playlist.js
@@ -92,6 +92,7 @@ class PlaylistController {
     }
 
     loadTopSongs(pageNum = 1, sortBy = null, callBack = null) {
+    	console.log('load topsongs ', pageNum);
     	
     	if(sortBy === null) {
     		sortBy = $page.myVues.playlist.menu.$data.SORTBY.SELECTED;
