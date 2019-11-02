4505101be233f8661c7ed737b0cce7e22487b920
Jonny Rimkus <jonny@rimkus.it>
2019-11-02 18:49:00 +0100

    testing

--- js/control/player/timer.js
+++ js/control/player/timer.js
@@ -13,7 +13,7 @@ class ChartTimer {
         this.timerRemaining = null;
         this.timerTrack = null;
         this.timer = null;
-        this.log = false;
+        this.log = true;
         this.lastChartLfmUser = null;
         this.player = player;
         this.init();
