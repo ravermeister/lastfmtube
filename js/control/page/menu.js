2eb469ec8e3dbcbdd0b9351e24cb7a3003b8ae1a
Jonny Rimkus <jonny@rimkus.it>
2019-11-07 00:39:54 +0100

    .

--- js/control/page/menu.js
+++ js/control/page/menu.js
@@ -68,51 +68,51 @@ class Menu {
     	
         if ('undefined' !== typeof json.listmenu) json = json.listmenu;
 
-//        let self = this;
-//    	let createHref = function(menu) {
-//
-//    		let page = null;
-//    		if(menu !== null && menu.PAGE !== null) {
-//    			page = self.pageControl.loader.pages.getByValue(menu.PAGE);    			
-//    		}else {
-//    			page = self.pageControl.loader.pages.base;
-//    		}
-//    		
-//    		return '#'+page.selector;
-//    	};
-//        
-//        if ('undefined' !== typeof json.YTPLAYER.TEXT) this.youtube.TEXT = json.YTPLAYER.TEXT;
-//        if ('undefined' !== typeof json.YTPLAYER.PAGE) {
-//        	this.youtube.PAGE = json.YTPLAYER.PAGE;
-//        	this.youtube.HREF = createHref(this.youtube);  
-//        }
-//
-//        if ('undefined' !== typeof json.LASTFM.TEXT) this.lastfm.TEXT = json.LASTFM.TEXT;
-//        if ('undefined' !== typeof json.LASTFM.PAGE) {
-//        	this.lastfm.PAGE = json.LASTFM.PAGE;
-//        	this.lastfm.HREF = createHref(this.lastfm);  
-//
-//        }
-//
-//        if ('undefined' !== typeof json.USERLIST.TEXT) this.userlist.TEXT = json.USERLIST.TEXT;
-//        if ('undefined' !== typeof json.USERLIST.PAGE) {
-//        	this.userlist.PAGE = json.USERLIST.PAGE;
-//        	this.userlist.HREF = createHref(this.userlist);  
-//
-//        }
-//
-//        if ('undefined' !== typeof json.TOPSONGS.TEXT) this.topsongs.TEXT = json.TOPSONGS.TEXT;
-//        if ('undefined' !== typeof json.TOPSONGS.PAGE) {
-//        	this.topsongs.PAGE = json.TOPSONGS.PAGE;
-//        	this.topsongs.HREF = createHref(this.topsongs);  
-//
-//        }
-//        
-//        if ('undefined' !== typeof json.TOPUSER.TEXT) this.topuser.TEXT = json.TOPUSER.TEXT;
-//        if ('undefined' !== typeof json.TOPUSER.PAGE) {
-//        	this.topuser.PAGE = json.TOPUSER.PAGE; 
-//        	this.topuser.HREF = createHref(this.topuser);  
-//        }
+        let self = this;
+    	let createHref = function(menu) {
+
+    		let page = null;
+    		if(menu !== null && menu.PAGE !== null) {
+    			page = self.pageControl.loader.pages.getByValue(menu.PAGE);    			
+    		}else {
+    			page = self.pageControl.loader.pages.base;
+    		}
+    		
+    		return '#'+page.selector;
+    	};
+        
+        if ('undefined' !== typeof json.YTPLAYER.TEXT) this.youtube.TEXT = json.YTPLAYER.TEXT;
+        if ('undefined' !== typeof json.YTPLAYER.PAGE) {
+        	this.youtube.PAGE = json.YTPLAYER.PAGE;
+        	this.youtube.HREF = createHref(this.youtube);  
+        }
+
+        if ('undefined' !== typeof json.LASTFM.TEXT) this.lastfm.TEXT = json.LASTFM.TEXT;
+        if ('undefined' !== typeof json.LASTFM.PAGE) {
+        	this.lastfm.PAGE = json.LASTFM.PAGE;
+        	this.lastfm.HREF = createHref(this.lastfm);  
+
+        }
+
+        if ('undefined' !== typeof json.USERLIST.TEXT) this.userlist.TEXT = json.USERLIST.TEXT;
+        if ('undefined' !== typeof json.USERLIST.PAGE) {
+        	this.userlist.PAGE = json.USERLIST.PAGE;
+        	this.userlist.HREF = createHref(this.userlist);  
+
+        }
+
+        if ('undefined' !== typeof json.TOPSONGS.TEXT) this.topsongs.TEXT = json.TOPSONGS.TEXT;
+        if ('undefined' !== typeof json.TOPSONGS.PAGE) {
+        	this.topsongs.PAGE = json.TOPSONGS.PAGE;
+        	this.topsongs.HREF = createHref(this.topsongs);  
+
+        }
+        
+        if ('undefined' !== typeof json.TOPUSER.TEXT) this.topuser.TEXT = json.TOPUSER.TEXT;
+        if ('undefined' !== typeof json.TOPUSER.PAGE) {
+        	this.topuser.PAGE = json.TOPUSER.PAGE; 
+        	this.topuser.HREF = createHref(this.topuser);  
+        }
         
         
     }
@@ -150,73 +150,73 @@ class Menu {
     getMenu(menuId) {
 
         let list = [];
-        switch (menuId) {
-        	case 'all':
-            case 'default':
-        		list = [
-                    this.youtube,
-                    this.lastfm,
-                    this.userlist,
-                    this.topsongs,
-                    this.topuser
-        		];
-        		break;
-            case 'youtube':
-            case 'video':
-            case 'video-youtube-container':
-            case 'video.youtube':
-                list = [
-                    this.lastfm,
-                    this.userlist,
-                    this.topsongs,
-                    this.topuser
-                ];
-                break;
-            case 'lastfm':
-            case 'playlist-lastfm-container':
-            case 'playlist.lastfm':
-                list = [
-                    this.youtube,
-                    this.userlist,
-                    this.topsongs,
-                    this.topuser
-                ];
-                break;
-            case 'topuser':
-            case 'user.topuser':
-            case 'user-topuser-container':
-            case 'search':
-                list = [
-                    this.youtube,
-                    this.lastfm,
-                    this.topsongs,
-                    this.userlist
-                ];
-                break;
-            case 'topsongs':
-            case 'playlist.topsongs':
-            case 'playlist-topsongs-container':
-                list = [
-                    this.youtube,
-                    this.lastfm,
-                    this.userlist,
-                    this.topuser
-                ];
-                break;
-            case 'userlist':
-            case 'playlist-user-container':
-            case 'playlist.user':
-                list = [
-                    this.youtube,
-                    this.lastfm,
-                    this.topsongs,
-                    this.topuser
-                ];
-                break;
-            default:
-                list = [];
-                break;
-        }
+//        switch (menuId) {
+//        	case 'all':
+//            case 'default':
+//        		list = [
+//                    this.youtube,
+//                    this.lastfm,
+//                    this.userlist,
+//                    this.topsongs,
+//                    this.topuser
+//        		];
+//        		break;
+//            case 'youtube':
+//            case 'video':
+//            case 'video-youtube-container':
+//            case 'video.youtube':
+//                list = [
+//                    this.lastfm,
+//                    this.userlist,
+//                    this.topsongs,
+//                    this.topuser
+//                ];
+//                break;
+//            case 'lastfm':
+//            case 'playlist-lastfm-container':
+//            case 'playlist.lastfm':
+//                list = [
+//                    this.youtube,
+//                    this.userlist,
+//                    this.topsongs,
+//                    this.topuser
+//                ];
+//                break;
+//            case 'topuser':
+//            case 'user.topuser':
+//            case 'user-topuser-container':
+//            case 'search':
+//                list = [
+//                    this.youtube,
+//                    this.lastfm,
+//                    this.topsongs,
+//                    this.userlist
+//                ];
+//                break;
+//            case 'topsongs':
+//            case 'playlist.topsongs':
+//            case 'playlist-topsongs-container':
+//                list = [
+//                    this.youtube,
+//                    this.lastfm,
+//                    this.userlist,
+//                    this.topuser
+//                ];
+//                break;
+//            case 'userlist':
+//            case 'playlist-user-container':
+//            case 'playlist.user':
+//                list = [
+//                    this.youtube,
+//                    this.lastfm,
+//                    this.topsongs,
+//                    this.topuser
+//                ];
+//                break;
+//            default:
+//                list = [];
+//                break;
+//        }
 
         return list;
     }
