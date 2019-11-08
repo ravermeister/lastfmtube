299542f04f8f32404817ac68fbd9d5630527d0d9
Jonny Rimkus <jonny@rimkus.it>
2019-11-08 21:28:36 +0100

    .

--- js/vueimpl/playlist/menu.js
+++ js/vueimpl/playlist/menu.js
@@ -56,7 +56,7 @@ class LibvuePlaylistMenu {
             		
             	},
                 loadPage: function (user, pageNum) {
-                	console.log($(document).children('#topsongs-sortby'));
+                	console.log($('#topsongs-sortby'));
             		let sortBy = $(this.$el).parent().children('#topsongs-sortby')
             			.children('option:selected').val();
             		console.log('sortby: ', sortBy);
