7e96c2966c613efdeb1c13c40712f6041426e84e
Jonny Rimkus <jonny@rimkus.it>
2019-11-09 00:54:03 +0100

    .

--- php/api/YouTube/YouTubeSearch.php
+++ php/api/YouTube/YouTubeSearch.php
@@ -178,7 +178,7 @@ class YouTubeSearch {
                }
           } catch (Exception $e) {
                Functions::getInstance()->logMessage('A service error occurred: ' . $e->getMessage());
-               $this->baseError($e->getMessage());
+               throw e;
           }
 
           return $video_list;
