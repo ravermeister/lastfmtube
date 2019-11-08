9d447fff6cb77055ed25136c7dc9f21cf790d9a2
Jonny Rimkus <jonny@rimkus.it>
2019-11-09 00:32:28 +0100

    .

--- js/control/page/loader.js
+++ js/control/page/loader.js
@@ -254,10 +254,9 @@ class PageLoader {
 				}
 				let self = this;
 				$playlist.loader.loadSearchResult(needle, pageNum, function(result, data){
-					console.log('needle', needle, 'result', result, 'data', data);
 					if(result) {	
+						console.log('needle', needle, 'result', result, 'data', data);
 						data.SEARCH_NEEDLE = needle;	
-						data.PLAYLIST = self.pageInfo.currentPage.value;
 						finished($page.myVues.playlist.search, data);												
 					}
 				});
