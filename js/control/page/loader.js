7215a007df0f5e678fefe1de856f64826371e386
Jonny Rimkus <jonny@rimkus.it>
2019-11-10 21:42:06 +0100

    .

--- js/control/page/loader.js
+++ js/control/page/loader.js
@@ -261,7 +261,7 @@ class PageLoader {
 				 * feels smooth.
 				 */
 				console.log('set timeout');
-				setTimeout(updateVueAndLoad, 500, vue, data, autoplay);
+				setTimeout(updateVueAndLoad, 250, vue, data, autoplay);
 			} else {				
 				updateVueAndLoad(vue, data, autoplay);
 			}
