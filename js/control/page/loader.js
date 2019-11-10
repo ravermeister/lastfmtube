0fd20ddf0a187666cc8f03282ce50e408b19cb9a
Jonny Rimkus <jonny@rimkus.it>
2019-11-10 02:10:32 +0100

    .

--- js/control/page/loader.js
+++ js/control/page/loader.js
@@ -202,9 +202,9 @@ class PageLoader {
 			self.setLocation('#'+page.selector);
 			if(vue !== null && data !== null) {		
 				vue.update(data);
-//				$page.myVues.updateAll({
-//					PLAYLIST: page.value
-//				});
+				$page.myVues.updateAll({
+					PLAYLIST: page.value
+				});
 			}
 		};  
 		
