72830c670d4ee642e3e63d9123b9430ac0e8a1c1
Jonny Rimkus <jonny@rimkus.it>
2019-11-13 21:48:15 +0100

    fix TRACK.NR in topsongs backend

--- php/json/page/Playlist.php
+++ php/json/page/Playlist.php
@@ -255,8 +255,6 @@ class Playlist extends DefaultJson {
           } else {
                $this->funcs->sortTracksByPlayCount($uniqueTracks, $offset);
           }
-
-          die(print_r($uniqueTracks));
           
           $maxpages = ((int) (sizeof($uniqueTracks) / $limit));
           if (($maxpages % $limit) > 0 || $maxpages <= 0) $maxpages ++;
