38015cb291aebd579818d26a25f9f2c2f5c2810c
Jonny Rimkus <jonny@rimkus.it>
2019-11-14 22:56:36 +0100

    .

--- js/vueimpl/player/header.js
+++ js/vueimpl/player/header.js
@@ -13,8 +13,8 @@ class LibvuePlayerHeader {
             el: '#'+elementId+'>h2',
             data: {
                 PLAYLIST: null,
-                CURRENT_TRACK: new TrackData(),
-                SEARCH_TRACK: new TrackData(),
+                CURRENT_TRACK: {},
+                SEARCH_TRACK: {},
                 LOADING: false
             },
             computed: {
