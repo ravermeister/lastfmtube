7a2b4e99d65d962cb257ef782bb9469ce84da006
Jonny Rimkus <jonny@rimkus.it>
2019-11-17 08:37:20 +0100

    .

--- js/vueimpl/page/page.js
+++ js/vueimpl/page/page.js
@@ -13,7 +13,7 @@ class LibvueMainpage {
         this.logo = new Vue({
             el: '#header>.logo',
             data: {
-                PAGE_LOADER: 'fa fa-spinner faa-spin animated fa-3x'
+                PAGE_LOADER: 'fas fa-atom faa-spin animated fa-3x'
             }
         });
 
