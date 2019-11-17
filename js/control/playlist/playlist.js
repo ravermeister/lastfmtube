a8f4ba6337f397de4aed0d1ff75c86de6758a72a
Jonny Rimkus <jonny@rimkus.it>
2019-11-17 05:11:14 +0100

    .

--- js/control/playlist/playlist.js
+++ js/control/playlist/playlist.js
@@ -220,7 +220,7 @@ class PlaylistController {
         		
         		if('undefined' === typeof track.ARTIST || 
         				'undefined' === typeof track.TITLE) {
-        			console.log('wie geht das verdammt???');
+        			console.log('undefined track: ', track);
         			continue;
         		}
         		
