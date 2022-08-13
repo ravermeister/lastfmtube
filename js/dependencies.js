ba6052829a504122283e6b9530434e4001dd347f
Jonny Rimkus <jonny@rimkus.it>
2022-08-13 08:40:16 +0200

    rever vue

--- js/dependencies.js
+++ js/dependencies.js
@@ -27,8 +27,8 @@ requirejs
 
 				// the Vue lib
 				Vue : [ 
-					'//unpkg.com/vue/dist/vue.global.prod',
-					'lib/vuejs/vue.global.prod'
+					'//unpkg.com/vue@2.7.8/dist/vue.min',
+					'lib/vuejs/vue.min'
 				],
 
 				// Vue RequireJS loader
